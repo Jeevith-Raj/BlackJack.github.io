80ef1e21990df652598da81fec2febba6dbae145
Jeevith-Raj <20uad028@kamarajengg.edu.in>
2024-05-14 15:23:33 -0400

    this is also a commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let firstCard = 6;
+let secondCard = 9;
+
+let sum = firstCard + secondCard;
+
+if(sum < 21)
+{
+    console.log("Do You want to draw a new card");
+}
+
+else if(sum === 21)
+{
+    console.log("Black Jack");
+}
+else if(sum > 21)
+{
+    console.log("You're out of the game");
+}
